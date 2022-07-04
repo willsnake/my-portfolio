e00be9001780d580ab7559f93626d402aae35a04
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2022-07-04 17:52:35 -0500

    fix(apps/web/src/components/Nav/Nav.svelte): Fixed hover state on the nav element

--- apps/web/src/components/Nav/Nav.svelte
+++ apps/web/src/components/Nav/Nav.svelte
@@ -5,22 +5,22 @@
     class="container mx-auto flex items-center content-center justify-between"
   >
     <a href="/">/ W I L L S N A K E</a>
-    <nav class="h-20">
+    <nav class="h-20 text-light-gray">
       <ul class="inline-table border-spacing-x-10 list-none h-full">
         <li class="table-cell text-center align-middle">
-          <a class="block" href="/">Home</a>
+          <a class="hover:text-dark-white" href="/">Home</a>
         </li>
         <li class="table-cell text-center align-middle">
-          <a class="block" href="#about">About Me</a>
+          <a class="hover:text-dark-white" href="#about">About Me</a>
         </li>
         <li class="table-cell text-center align-middle">
-          <a class="block" href="#projects">Projects</a>
+          <a class="hover:text-dark-white" href="#projects">Projects</a>
         </li>
         <li class="table-cell text-center align-middle">
-          <a class="block" href="#blog">Blog</a>
+          <a class="hover:text-dark-white" href="#blog">Blog</a>
         </li>
         <li class="table-cell text-center align-middle">
-          <a class="block" href="#contact">Contact</a>
+          <a class="hover:text-dark-white" href="#contact">Contact</a>
         </li>
       </ul>
     </nav>
