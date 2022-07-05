270af73efb2577bf90ada9589c230a61ecc58999
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2022-07-04 19:14:04 -0500

    feat(apps/web/src/components/H1/H1.svelte): Created new H1 component

---
+++ apps/web/src/components/H1/H1.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  const rest = { ...$$props };
+</script>
+
+<h1 {...rest} class={`font-medium text-dark-white`}>
+  <slot />
+</h1>
+
+<style>
+  h1 {
+    font-size: 40px;
+    line-height: 52px;
+  }
+</style>
