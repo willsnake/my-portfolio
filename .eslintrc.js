763b912227bf4665660f3353108d939ad318d992
WillSnake <willsnake87@gmail.com>
2023-02-14 21:51:56 -0600

    feat: Updated config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,166 @@
 module.exports = {
-  root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/"],
-    },
+  // Configuration for JavaScript files
+  env: {
+    node: true,
+    es2022: true,
+    browser: true,
   },
+  extends: [
+    "eslint:recommended",
+    "plugin:astro/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: true,
+        endOfLine: "auto",
+      },
+    ],
+  },
+  overrides: [
+    // Configuration for mjs files
+    {
+      files: ["**/*.mjs"],
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: 2015,
+      },
+      extends: ["plugin:prettier/recommended"],
+      rules: {
+        "prettier/prettier": [
+          "error",
+          {
+            singleQuote: true,
+            endOfLine: "auto",
+          },
+        ],
+        "import/no-extraneous-dependencies": "off", // mjs is only used by Astro for configuration, false positive
+        "import/no-unresolved": "off", // Also false positive with mjs file
+      },
+    },
+    {
+      files: ["**/*.astro"],
+      plugins: [
+        "@typescript-eslint",
+        "unused-imports",
+        "tailwindcss",
+        "simple-import-sort",
+      ],
+      extends: [
+        "plugin:tailwindcss/recommended",
+        "airbnb-typescript",
+        "plugin:prettier/recommended",
+      ],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        project: "./tsconfig.json",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        "prettier/prettier": [
+          "error",
+          {
+            singleQuote: true,
+            endOfLine: "auto",
+          },
+        ],
+        "import/extensions": [
+          "error",
+          "ignorePackages",
+          {
+            js: "never",
+            jsx: "never",
+            ts: "never",
+            tsx: "never",
+            svelte: "never",
+            "": "never",
+          },
+        ], // Avoid missing file extension errors in .astro files
+        "import/no-unresolved": [
+          "error",
+          {
+            ignore: ["@/*"],
+          },
+        ], // Disable no-unresolved rule for .astro files
+        "@typescript-eslint/comma-dangle": "off", // Avoid conflict rule between Eslint and Prettier
+        "@typescript-eslint/consistent-type-imports": "error", // Ensure `import type` is used when it's necessary
+        "import/prefer-default-export": "off", // Named export is easier to refactor automatically
+        "tailwindcss/classnames-order": [
+          "warn",
+          {
+            officialSorting: true,
+          },
+        ], // Follow the same ordering as the official plugin `prettier-plugin-tailwindcss`
+        "simple-import-sort/imports": "error", // Import configuration for `eslint-plugin-simple-import-sort`
+        "simple-import-sort/exports": "error", // Export configuration for `eslint-plugin-simple-import-sort`
+        "@typescript-eslint/no-unused-vars": "off",
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      },
+      globals: {
+        Astro: "readonly",
+      },
+    },
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      plugins: [
+        "@typescript-eslint",
+        "unused-imports",
+        "tailwindcss",
+        "simple-import-sort",
+      ],
+      extends: [
+        "plugin:tailwindcss/recommended",
+        "plugin:prettier/recommended",
+      ],
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      rules: {
+        "prettier/prettier": [
+          "error",
+          {
+            singleQuote: true,
+            endOfLine: "auto",
+          },
+        ],
+        "import/extensions": [
+          "error",
+          "ignorePackages",
+          {
+            js: "never",
+            jsx: "never",
+            ts: "never",
+            tsx: "never",
+            "": "never",
+          },
+        ], // Avoid missing file extension errors when using '@/' alias
+        "@typescript-eslint/comma-dangle": "off", // Avoid conflict rule between Eslint and Prettier
+        "@typescript-eslint/consistent-type-imports": "error", // Ensure `import type` is used when it's necessary
+        "import/prefer-default-export": "off", // Named export is easier to refactor automatically
+        "tailwindcss/classnames-order": [
+          "warn",
+          {
+            officialSorting: true,
+          },
+        ], // Follow the same ordering as the official plugin `prettier-plugin-tailwindcss`
+        "simple-import-sort/imports": "error", // Import configuration for `eslint-plugin-simple-import-sort`
+        "simple-import-sort/exports": "error", // Export configuration for `eslint-plugin-simple-import-sort`
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+        ],
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      },
+    },
+  ],
 };
