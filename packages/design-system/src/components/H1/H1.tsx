d4ff4baa980c3747ec037371d6ebec6247a38b9f
Daniel Rodríguez Monroy <willsnake87@gmail.com>
2025-06-22 10:34:37 -0600

    Adds H1 component to the design system
    Adds an H1 component to the design system.
    
    This provides a consistent and styled heading element
    for use throughout the application, improving
    maintainability and visual consistency.

---
+++ packages/design-system/src/components/H1/H1.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent } from 'preact';
+import { cv } from 'css-variants'
+import type { H1Props } from './types';
+
+/**
+ * H1 component
+ * 
+ * @param props - H1 properties
+ * @returns H1 component
+ */
+export const H1: FunctionComponent<H1Props> = ({
+    className = '',
+    children,
+    ...rest
+}) => {
+
+    const classNames = cv({
+        base: 'font-medium text-dark-white text-[40px] leading-[52px]',
+    })
+
+    return (
+        <h1
+            {...rest}
+            class={classNames({ className })}
+        >
+            {children}
+        </h1>
+    );
+};
