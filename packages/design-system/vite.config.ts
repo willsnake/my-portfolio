e8510e3a101a6ad28c0a90f9bca123b839694e15
Daniel Rodríguez Monroy <willsnake87@gmail.com>
2025-07-05 20:42:43 -0600

    feat: :wrench: Updated vite config

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'node:path';
 import preact from '@preact/preset-vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
@@ -5,4 +6,19 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact(), tailwindcss()],
+  build: {
+    lib: {
+      entry: resolve(import.meta.dirname, 'src/index.ts'),
+      name: 'WSDesignSystem',
+      fileName: (format) => `design-system.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['preact'],
+      output: {
+        globals: {
+          preact: 'preact',
+        },
+      },
+    },
+  },
 });
